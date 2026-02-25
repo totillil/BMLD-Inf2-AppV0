5c722afd896be6534b9fe9a86211ac02a1b6f917
Lilia <totillil@students.zhaw.ch>
2026-02-25 08:26:22 +0100

    Added my email

--- views/home.py
+++ views/home.py
@@ -6,8 +6,8 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Lilia Totila (totillil@zhaw.ch)
+- 
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
