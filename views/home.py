b347a1388de16b754c4480c4bd5ae0dfffa909fd
sahitero <eronita.sahiti04@gmail.com>
2026-02-26 18:15:33 +0100

    gänsefüsschen wurden vergessen

--- views/home.py
+++ views/home.py
@@ -10,3 +10,4 @@ Diese App wurde von folgenden Personen entwickelt:
 - Eronita Sahiti (sahitero@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+"""
