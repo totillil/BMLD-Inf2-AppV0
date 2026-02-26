b7387534dbf5a247cf42b63000ce30d4566169af
sahitero <eronita.sahiti04@gmail.com>
2026-02-26 11:54:22 +0100

    Update home.py

--- views/home.py
+++ views/home.py
@@ -7,7 +7,7 @@ st.title("Meine erste Streamlit App")
 """
 Diese App wurde von folgenden Personen entwickelt:
 - Lilia Totila (totillil@zhaw.ch)
-- 
+- Eronita Sahiti (sahitero@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
