368b2e8ebab675b39a1cc1545b8dfdc30e5aa2f1
sahitero <eronita.sahiti04@gmail.com>
2026-03-11 08:34:49 +0100

    Probieren ob wir eine Neue Unterseite erstellen können

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
 pg_second = st.Page("views/GFR-Rechner.py", title="GFR-Rechner", icon=":material/info:")
+pg_third = st.Page("views/Neue-App.py", title="Neue App", icon=":material/info:")
 
-pg = st.navigation([pg_home, pg_second])
+pg = st.navigation([pg_home, pg_second, pg_third])
 pg.run()
