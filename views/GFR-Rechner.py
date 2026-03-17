1d0ceadaf5c27dfad65f49de55ba26ba2127eb5a
Lilia <totillil@students.zhaw.ch>
2026-03-17 23:41:17 +0100

    Verbesserung des Codes, durch Bot in VS Code, da es eine Fehlermeldung gab, er hatt dies korrigiert

--- views/GFR-Rechner.py
+++ views/GFR-Rechner.py
@@ -19,22 +19,7 @@ with col2:
 if einheit == "µmol/l":
     kreatinin = kreatinin / 88.4
 
-#Das isch unseri formle
-def calculate_gfr(krea, age, sex):
-    #do werde die faktore beobachtet und gändert will zb Fraue im schnitt weniger Muskelmasse hänt und somit weniger Kreatinin im Blut
-    if sex == "Weiblich":
-        kappa = 0.7
-        alpha = -0.329
-        gender_fix = 1.018
-    else:
-        kappa = 0.9
-        alpha = -0.411
-        gender_fix = 1.0
-
-    gfr = 141 * min(krea/kappa, 1)**alpha * max(krea/kappa, 1)**-1.209 * 0.993**age * gender_fix
-    return gfr
-
-# Das zeigt das ergebnis ah 
+# Das zeigt das ergebnis ah
 if st.button("GFR berechnen"):
     result = calculate_gfr(kreatinin, alter, geschlecht)
     
