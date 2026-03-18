ad2c9baa66d212cf80d9f2f04e4677104af5c1a8
Lilia <totillil@students.zhaw.ch>
2026-03-18 08:27:48 +0100

    Einfügen des Datta Managr in der App.py Code und dem login manager

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 import streamlit as st
 import pandas as pd 
 
+# --- NEW CODE: import and initialize data manager and login manager ---
+from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
+
+data_manager = DataManager(       # initialize data manager
+    fs_protocol='webdav',         # protocol for the filesystem, use webdav for switch drive
+    fs_root_folder="BMLD Informatik 2 Eronita, Lilia"  # folder on switch drive where the data is stored
+    ) 
+login_manager = LoginManager(data_manager) # handles user login and registration
+login_manager.login_register()             # stops if not logged in
+# --- END OF NEW CODE ---
+
+# --- CODE UPDATE: load user data from data manager if not already present in session state --
+if 'data_df' not in st.session_state:
+    st.session_state['data_df'] = data_manager.load_user_data(
+        'data.csv',                     # The file on switch drive where the data is stored
+        initial_value=pd.DataFrame(),   # Initial value if the file does not exist
+        parse_dates=['timestamp']       # Parse timestamp as datetime
+    )
+# --- END OF CODE UPDATE ---
+
 if 'data_df' not in st.session_state:
     st.session_state['data_df'] = pd.DataFrame()
 
