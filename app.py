7e147e5a5a393ff785b4771cee65fe782000f328
Lilia <totillil@students.zhaw.ch>
2026-03-25 09:36:17 +0100

    updated

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import pandas as pd
 from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
 
+st.set_page_config(page_title="Meine App", page_icon=":material/home:")
+
 data_manager = DataManager(       # initialize data manager
     fs_protocol='webdav',         # protocol for the filesystem, use webdav for switch drive
-    fs_root_folder="BMLD Informatik 2 Eronita, Lilia"  # folder on switch drive where the data is stored
+    fs_root_folder="BMLD_Informatik"  # folder on switch drive where the data is stored
     ) 
 login_manager = LoginManager(data_manager) # handles user login and registration
 login_manager.login_register()             # stops if not logged in
@@ -25,7 +27,7 @@ if 'data_df' not in st.session_state:
 if 'data_df' not in st.session_state:
     st.session_state['data_df'] = pd.DataFrame()
 
-st.set_page_config(page_title="Meine App", page_icon=":material/home:")
+
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
 pg_second = st.Page("views/GFR-Rechner.py", title="GFR-Rechner", icon=":material/info:")
